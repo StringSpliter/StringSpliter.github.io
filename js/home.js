2f30e9033a693747bb8bce45c2a9d8d6f683371d
StringSpliter <1436474557@qq.com>
2017-03-16 23:36:29 +0800

    home.js

---
+++ js/home.js
@@ -0,0 +1,13 @@
+$(function(){
+	homeObject.navbarClickInit();
+})
+
+var homeObject={
+	navbarClickInit:function(){
+		$("#home_navbar a").click(function(){
+			$("#home_navbar").find("li").removeClass("active");
+			$(this).parent().addClass("active");
+		});
+	},
+
+}
